17d55d5b1b6c1ba3d14e151014f9685244b9d06f
omerxx <omerxx@gmail.com>
2018-02-09 00:48:31 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# drone-lambda update environment plugin
+
+#### This plugin is created in order to allow drone secret interpolation and injection into Lambda env variables
+
+TODO: 
+1. Add KMS Eneryption!
+2. CI
+
+
+
---
+++ lambda-update-env.py
@@ -0,0 +1,55 @@
+#!/bin/python
+
+import os
+import boto3
+
+def pp(name):
+    # pp as in plugin parameter 
+    param = os.environ.get('{}_{}'.format('PLUGIN', name.upper()))
+    if param:
+        return param
+    else:
+        print 'No such variable {}'.format(name)
+        return None
+
+
+def env_handler(paramString):
+    omap = []
+    envSets = paramString.split(',')
+    for set in envSets:
+        value = os.environ.get(set.split('=')[0]) if not set.split('=')[1] and os.environ.get(set.split('=')[0]) else set.split('=')[1]
+            
+        omap.append(
+            {
+                'name': set.split('=')[0],
+                'value': value
+            }
+        )
+
+    return omap  
+
+
+def update_function_configuration():
+    response = client.update_function_configuration(
+        FunctionName = pp('functionname'),
+        Environment={
+            'Variables': env_handler(pp(environment_variables))
+        }
+    )
+
+
+ACCESS_KEY = pp('access_key')
+SECRET_KEY = pp('secret_key')
+AWS_REGION = pp('region')
+
+
+if __name__ == "__main__":
+    global client 
+    client = boto3.client(
+        'lambda',
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+        region_name=AWS_REGION
+    )
+    
+ 
